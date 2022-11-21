56142a442192f9e44d6680080e613fb843283622
artem.korotenko <artem.korotenko@betterme.world>
2022-11-22 00:37:21 +0200

    updated validation

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ def is_game_over():
     return guessed == WORDS_TO_WIN or errors == ERRORS_TO_LOSE
 
 
+def guess_is_valid(candidate):
+    for letter in candidate:
+        if letter not in word:
+            print(f"You can not use letter {letter}")
+            return False
+        count = word.count(letter)
+        if count < candidate.count(letter):
+            print(f"You can use letter {letter} only {count} times")
+            return False
+    return True
+
+
 guessed = 0
 errors = 0
 
@@ -28,6 +40,10 @@ print(f"Your word is '{word}'")
 
 while not is_game_over():
     guess = input("Your next take: ")
+
+    if not guess_is_valid(guess):
+        continue
+
     if guess in full_list:
         guessed += 1
         guesses.append(guess)
